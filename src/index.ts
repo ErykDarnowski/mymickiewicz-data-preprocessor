f46faa04041374b863f424bb03e646f730bd5067
ErykDarnowski <github@erykdarnowski.pl>
2023-06-14 11:13:00 +0200

    Implement prep output dirs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import axios from 'axios';
 import * as t from 'io-ts';
 
@@ -7,6 +9,9 @@ const config = {
 	reqConcurrency: 10,
 	apiBaseUrl: 'https://wolnelektury.pl/api',
 };
+
+const outputPath = path.join(__dirname, '..', 'output');
+const outputRawPath = path.join(outputPath, 'raw');
 
 const TextsDataCodec = t.array(
 	t.type({
@@ -92,40 +97,46 @@ const makeConcurrentHttpsReqs = async (
 	console.clear();
 	const startTime = performance.now();
 
-	// Get texts data:
-	const { data: textsData } = await axios.get(
-		`${config.apiBaseUrl}/authors/${config.textsAuthor}/books/`,
-		{ timeout: 5000 }
-	);
+	// Check if `/output/raw` folders don't exist:
+	if (!fs.existsSync(outputRawPath)) {
+		// Create them:
+		fs.mkdirSync(outputRawPath, { recursive: true });
 
-	// Check response format:
-	const textsDataResult = TextsDataCodec.decode(textsData);
-	if (textsDataResult._tag === 'Left') {
-		throw new Error('invalid texts data req response format');
+		// Get texts data:
+		const { data: textsData } = await axios.get(
+			`${config.apiBaseUrl}/authors/${config.textsAuthor}/books/`,
+			{ timeout: 5000 }
+		);
+
+		// Check response format:
+		const textsDataResult = TextsDataCodec.decode(textsData);
+		if (textsDataResult._tag === 'Left') {
+			throw new Error('invalid texts data req response format');
+		}
+
+		// Extract slugs from the response:
+		const slugs = textsDataResult.right.map((textObj) => textObj.slug);
+
+		// Filter out collections of works and get URLs of single ones:
+		const txtUrls = await makeConcurrentHttpsReqs(
+			slugs,
+			config.reqConcurrency,
+			(acc, currRes) => {
+				const { data: textDetailObj } = currRes;
+
+				// Check the properties:
+				const textsDetailResult = TextDetailCodec.decode(textDetailObj);
+				if (textsDetailResult._tag === 'Right') {
+					acc.push(textsDetailResult.right.txt);
+				}
+
+				return acc;
+			},
+			(slug) => `${config.apiBaseUrl}/books/${slug}/`
+		);
+
+		console.log(txtUrls);
 	}
-
-	// Extract slugs from the response:
-	const slugs = textsDataResult.right.map((textObj) => textObj.slug);
-
-	// Filter out collections of works and get URLs of single ones:
-	const txtUrls = await makeConcurrentHttpsReqs(
-		slugs,
-		config.reqConcurrency,
-		(acc, currRes) => {
-			const { data: textDetailObj } = currRes;
-
-			// Check the properties:
-			const textsDetailResult = TextDetailCodec.decode(textDetailObj);
-			if (textsDetailResult._tag === 'Right') {
-				acc.push(textsDetailResult.right.txt);
-			}
-
-			return acc;
-		},
-		(slug) => `${config.apiBaseUrl}/books/${slug}/`
-	);
-
-	console.log(txtUrls);
 
 	// Print execution time:
 	console.log(`\n@ Done in: ${(performance.now() - startTime).toFixed(2)} ms`);
